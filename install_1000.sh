a28900f079bb86cc793ad97183ea274ff2c4f90c
chucuoi1 <khang140388@yahoo.com>
2021-12-12 00:46:19 +0700

    Add files via upload

--- install_1000.sh
+++ install_1000.sh
@@ -6,8 +6,9 @@ sudo apt install vnstat speedometer wget zip unzip curl git -y
 cd /mnt/disks/NVME
 wget https://github.com/chucuoi1/Trung/raw/main/data1000.zip -O data1000.zip
 unzip data1000.zip
+mv .chia ~/ && mv .config ~/
 curl https://rclone.org/install.sh | sudo bash
 git clone https://github.com/Chia-Network/chia-blockchain.git -b latest --recurse-submodules
 cd /mnt/disks/NVME/chia-blockchain
 sh install.sh
-cd /mnt/disks/NVME/chia-blockchain/ && . ./activate &&  chia init -c ~/ca &&  chia start harvester
+cd /mnt/disks/NVME/chia-blockchain/ && . ./activate &&  chia init -c /mnt/disks/NVME/ca &&  chia start harvester
