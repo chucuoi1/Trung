d9f3960b7e920b2e452695032c70f93f57a11831
chucuoi1 <khang140388@yahoo.com>
2021-12-23 08:39:46 +0700

    Update and rename install_1000.sh to install1000.sh

--- install1000.sh
+++ install1000.sh
@@ -3,12 +3,10 @@ sudo apt-get update -y
 wait
 sudo apt-get upgrade -y
 sudo apt install vnstat speedometer wget zip unzip curl git -y
-cd /mnt/disks/NVME
 wget https://github.com/chucuoi1/Trung/raw/main/data1000.zip -O data1000.zip
 unzip data1000.zip
-mv .chia ~/ && mv .config ~/
 curl https://rclone.org/install.sh | sudo bash
 git clone https://github.com/Chia-Network/chia-blockchain.git -b latest --recurse-submodules
-cd /mnt/disks/NVME/chia-blockchain
+cd ~/chia-blockchain
 sh install.sh
-cd /mnt/disks/NVME/chia-blockchain/ && . ./activate &&  chia init -c /mnt/disks/NVME/ca &&  chia start harvester
+cd ~/chia-blockchain/ && . ./activate &&  chia init -c ~/ca &&  chia start harvester
